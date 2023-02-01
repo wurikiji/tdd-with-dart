4e8f0013e0ed46ed3a674f558bfbdf30df376780
wurikiji <wurikiji@gmail.com>
2023-02-01 23:10:31 +0900

    테스트코드 작성에서의 위험성 커멘트

--- tdd-money/test/some_test.dart
+++ tdd-money/test/some_test.dart
@@ -5,6 +5,8 @@ void main() {
   group('Dollar', () {
     test('can multiply a number', () {
       final five = Dollar(5);
+      // 아래의 동치성 테스트 코드가 잘못된 검증을 하고 있다면
+      // 이 코드들도 잘못된 검증을 하게 됨.
       expect(five.times(2), equals(Dollar(10)));
       expect(five.times(3), equals(Dollar(15)));
     });
