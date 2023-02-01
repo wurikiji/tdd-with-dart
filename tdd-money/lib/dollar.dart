7b598fda85b04cefd54fb6a6f8b1e31cf3362709
wurikiji <wurikiji@gmail.com>
2023-02-01 23:10:31 +0900

    컴파일이 되도록 수정

--- tdd-money/lib/dollar.dart
+++ tdd-money/lib/dollar.dart
@@ -2,9 +2,9 @@ class Dollar {
   Dollar(this.amount);
 
   int amount;
-  void times(int multiplier) {
-    // * 2 라는 테스트/구현 사이의 중복을 제거
-    // 중복 제거를 위해 amount = amount * multiplier 가 아닌 *= 오퍼레이터를 사용
+  Dollar times(int multiplier) {
     amount *= multiplier;
+    // 컴파일이 되도록 수정
+    return this;
   }
 }
