a4a065ca4db78958748e67a32ebbc0e32a45d391
wurikiji <wurikiji@gmail.com>
2023-02-01 23:10:31 +0900

    refactor first test 2

--- tdd-money/lib/dollar.dart
+++ tdd-money/lib/dollar.dart
@@ -3,6 +3,8 @@ class Dollar {
 
   int amount;
   void times(int multiplier) {
-    amount *= 2;
+    // * 2 라는 테스트/구현 사이의 중복을 제거
+    // 중복 제거를 위해 amount = amount * multiplier 가 아닌 *= 오퍼레이터를 사용
+    amount *= multiplier;
   }
 }
