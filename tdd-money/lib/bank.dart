cf42a4de458fdb11fdd8257005f07ee9ba33af0e
wurikiji <wurikiji@gmail.com>
2023-02-06 20:21:53 +0900

    impl: 최소한의 기능 구현

--- tdd-money/lib/bank.dart
+++ tdd-money/lib/bank.dart
@@ -3,6 +3,7 @@ import 'package:tdd_money/money.dart';
 
 class Bank {
   Money reduce(Expression source, String to) {
+    if (source is Money) return source;
     final sum = source as Sum;
     return sum.reduce(to);
   }
