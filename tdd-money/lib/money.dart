cc98ecaa9f0516f7d64fa3fc6846debe41c1319c
wurikiji <wurikiji@gmail.com>
2023-02-06 16:49:42 +0900

    impl: 테스트 통과를 위한 팩토리 메서드 추가

--- tdd-money/lib/money.dart
+++ tdd-money/lib/money.dart
@@ -1,5 +1,6 @@
 import 'package:meta/meta.dart';
 import 'package:tdd_money/dollar.dart';
+import 'package:tdd_money/franc.dart';
 
 abstract class Money {
   const Money(this.amount);
@@ -7,6 +8,7 @@ abstract class Money {
   final int amount;
 
   factory Money.dollar(int amount) => Dollar(amount);
+  factory Money.franc(int amount) => Franc(amount);
 
   Money times(int multiplier);
 
