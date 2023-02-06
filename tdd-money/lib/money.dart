a73d543bef854585df0446621e519851a0f1b909
wurikiji <wurikiji@gmail.com>
2023-02-06 16:45:38 +0900

    impl: 테스트를 통과하는 최소한의 코드

--- tdd-money/lib/money.dart
+++ tdd-money/lib/money.dart
@@ -1,12 +1,14 @@
 import 'package:meta/meta.dart';
 import 'package:tdd_money/dollar.dart';
 
-class Money {
+abstract class Money {
   const Money(this.amount);
   @protected
   final int amount;
 
   factory Money.dollar(int amount) => Dollar(amount);
+
+  Money times(int mupltiplier);
 
   @override
   bool operator ==(Object? other) {
