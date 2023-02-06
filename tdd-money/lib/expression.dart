781dfca8eef93d2e1eca32e477c253a335f994ee
wurikiji <wurikiji@gmail.com>
2023-02-06 20:41:33 +0900

    impl: 통과하는 구현

--- tdd-money/lib/expression.dart
+++ tdd-money/lib/expression.dart
@@ -15,7 +15,8 @@ class Sum extends Expression {
 
   @override
   Money reduce(Bank bank, String to) {
-    final amount = augend.amount + addend.amount;
+    final amount =
+        augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
     return Money(amount, to);
   }
 }
