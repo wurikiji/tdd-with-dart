16900e786b6220b984ec234b1df4cc2c3c91db88
wurikiji <wurikiji@gmail.com>
2023-02-06 17:52:03 +0900

    refactor: 불필요한 클래스 제거

--- tdd-money/lib/dollar.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tdd_money/money.dart';
-
-class Dollar extends Money {
-  const Dollar(super.amount, [super._currency = "USD"]);
-}
--- tdd-money/lib/franc.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tdd_money/money.dart';
-
-class Franc extends Money {
-  const Franc(super.amount, [super._currency = "CHF"]);
-}
