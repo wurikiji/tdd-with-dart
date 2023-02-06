7aace422a0bf20d31e410ae22841af03d50def1c
wurikiji <wurikiji@gmail.com>
2023-02-06 17:45:50 +0900

    refactor: Money 클래스와 하위 클래스 의존성 제거

--- tdd-money/lib/money.dart
+++ tdd-money/lib/money.dart
@@ -1,6 +1,4 @@
 import 'package:meta/meta.dart';
-import 'package:tdd_money/dollar.dart';
-import 'package:tdd_money/franc.dart';
 
 class Money {
   const Money(this.amount, this._currency);
@@ -8,8 +6,8 @@ class Money {
   final int amount;
   final String _currency;
 
-  factory Money.dollar(int amount) => Dollar(amount, "USD");
-  factory Money.franc(int amount) => Franc(amount, "CHF");
+  factory Money.dollar(int amount) => Money(amount, "USD");
+  factory Money.franc(int amount) => Money(amount, "CHF");
 
   Money times(int multiplier) {
     return Money(amount * multiplier, currency());
